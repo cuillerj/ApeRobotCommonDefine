355c035fb430b0d8073cc17734e5f8e8d554fc25
cuillerj <jean.cuiller@wanadoo.fr>
2016-06-18 16:02:50 +0200

    ApeRobot definition to be shared with Octave code

---
+++ ApeRobotCommonDefine.h
@@ -0,0 +1,21 @@
+#define minDistToBeDone 3  // cm
+#define minRotToBeDone 5  // degree
+#define scanning 102   // 0x66
+#define scanEnded 103  // 0x67
+#define moving 104     // 0x68
+#define moveEnded 105  // 0x69
+#define aligning 106  // 0x6a
+#define alignEnded 107 //0x6b
+#define moveRetcodeEncoderLeftLowLevel 1
+#define moveRetcodeEncoderRightLowLevel 2
+#define moveRetcodeEncoderLeftHighLevel 3
+#define moveRetcodeEncoderRightHighLevel 4
+#define moveUnderLimitation 5
+#define moveKoDueToSpeedInconsistancy 6
+#define moveKoDueToObstacle 7
+#define diagMotorPbLeft 0
+#define diagMotorPbRight 1
+#define diagMotorPbSynchro 2
+#define diagMotorPbEncoder 3
+#define diagRobotPause 0
+#define diagRobotObstacle 1 
